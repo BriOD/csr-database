d5ea3c38f6f910107fbef73784b71b25354faf65
arturusfury <bri.reynolds@gmail.com>
2016-08-20 11:48:52 -0500

    refactored devise find_for_google method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,26 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   # :recoverable, :rememberable, :trackable
   devise :database_authenticatable, :registerable, :validatable
-
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.find_for_google_oauth2(access_token, signed_in_resource = nil)
     data = access_token.info
+
     user = User.where(provider: access_token.provider, uid: access_token.uid).first
-    if user
-      return user
-    else
-      registered_user = User.where(email: access_token.info.email).first
-      if registered_user
-        return registered_user
-      else
-        user = User.create(name: data['name'],
-          provider:access_token.provider,
-          email: data['email'],
-          uid: access_token.uid ,
-          password: Devise.friendly_token[0,20],
-        )
-      end
-    end
+    return user if user
+
+    registered_user = User.where(email: access_token.info.email).first
+    return registered_user if registered_user
+
+    User.create(
+      name: data['name'],
+      provider: access_token.provider,
+      email: data['email'],
+      uid: access_token.uid,
+      password: Devise.friendly_token[0, 20]
+    )
   end
 end
