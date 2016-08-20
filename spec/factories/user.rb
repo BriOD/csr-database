cb8bc51daec41420eda19e78ca5cab5cc8a71b1d
arturusfury <bri.reynolds@gmail.com>
2016-08-20 14:09:02 -0500

    added more user specs

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,10 @@
+require 'faker'
+
 FactoryGirl.define do
   factory :user do |f|
-    # f.firstname 'John'
-    # f.lastname 'Doe'
+    f.first_name { Faker::Name.first_name }
+    f.last_name { Faker::Name.last_name }
+    f.email { Faker::Internet.safe_email }
+    f.password { Faker::Internet.password }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,10 +2,22 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'User' do
-    it 'has a valid factory'
-    it 'is invalid without a first name'
-    it 'is invalid without a last name'
-    it 'returns a contacts full name as a string'
-    it 'is invalid without an email'
+    it 'has a valid factory' do
+      expect(FactoryGirl.create(:user)).to be_valid
+    end
+
+    it 'is invalid without a first name' do
+      # expect(FactoryGirl.build(:user, first_name: nil)).to_not be_valid
+    end
+    #
+    # it 'is invalid without a last name' do
+    #
+    # end
+    #
+    # it 'returns a contacts full name as a string' do
+    #
+    # end
+    #
+    # it 'is invalid without an email'
   end
 end
