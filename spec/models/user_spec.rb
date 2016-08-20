2964b4bd5397cee9119ff0b294e464a4eea30665
arturusfury <bri.reynolds@gmail.com>
2016-08-20 13:51:34 -0500

    began adding model tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'User' do
+    it 'has a valid factory'
+    it 'is invalid without a first name'
+    it 'is invalid without a last name'
+    it 'returns a contacts full name as a string'
+    it 'is invalid without an email'
+  end
 end
