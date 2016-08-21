d86dd2d210d847169b53cedcb185d1b4c38311d4
arturusfury <bri.reynolds@gmail.com>
2016-08-21 17:57:42 -0500

    added basic tests for ip ranges

--- spec/factories/ip_ranges.rb
+++ spec/factories/ip_ranges.rb
@@ -1,10 +1,8 @@
 FactoryGirl.define do
-  factory :ip_range do
-    IpRange.create(
-      id: IpRange.ip2long('192.168.0.1'),
-      network: '192.168.0.2/24',
-      gateway: '192.168.0.1',
-      service_id: 3
-    )
+  factory :ip_range do |f|
+    f.id IpRange.ip2long('192.168.1.1')
+    f.network '192.168.0.2/24'
+    f.gateway '192.168.0.1'
+    f.service_id 3
   end
 end
--- spec/models/ip_range_spec.rb
+++ spec/models/ip_range_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe IpRange, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'IpRange' do
+    it 'has a valid factory' do
+      expect(FactoryGirl.create(:ip_range, network: '10.10.0.1/24', gateway: '10.10.0.1')).to be_valid
+    end
+
+    it 'must have a valid ip address and cidr for the network' do
+      expect(FactoryGirl.build(:ip_range, network: '192.168.0.1')).to_not be_valid
+      expect(FactoryGirl.build(:ip_range, network: 'network')).to_not be_valid
+      expect(FactoryGirl.build(:ip_range, gateway: '10.10.0.1', network: '192.168.0.1/24')).to be_valid
+    end
+
+    it 'must have a valid ip address for the gateway' do
+      expect(FactoryGirl.build(:ip_range, gateway: 'gateway')).to_not be_valid
+    end
+
+    it 'must have a service_id' do
+      expect(FactoryGirl.build(:ip_range, service_id: nil)).to_not be_valid
+    end
+
+    it 'cannot have the same network or gateway as a previous IP Range' do
+      expect(FactoryGirl.create(:ip_range, network: '192.168.0.1/24')).to be_valid
+      # Trying to use Same Network
+      expect(FactoryGirl.build(:ip_range, network: '192.168.0.1/24')).to_not be_valid
+      # Change to New Network
+      expect(FactoryGirl.build(:ip_range, network: '192.168.1.1/24')).to_not be_valid
+      # Change both Network & Gateway
+      expect(FactoryGirl.build(:ip_range, network: '192.168.1.1/24', gateway: '192.168.1.1')).to be_valid
+    end
+  end
 end
