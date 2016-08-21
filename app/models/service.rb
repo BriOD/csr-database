548db827b3303c0e80a305c1471f6149b783bf47
arturusfury <bri.reynolds@gmail.com>
2016-08-21 15:30:07 -0500

    added comments with table information

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,8 @@
+# ==============================================================================
+# Services Table
+#
+# id, icon, main_type, main_name, sub_type, sub_name
+# ==============================================================================
 class Service < ApplicationRecord
   validates_presence_of :icon, :main_type, :main_name
   validates_uniqueness_of :sub_type, :sub_name, scope: :main_type
