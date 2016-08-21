163ba189bb1aed9d3a2cb30955db525d13e4a375
arturusfury <bri.reynolds@gmail.com>
2016-08-21 17:58:13 -0500

    added validations for our ip ranges

--- app/models/ip_range.rb
+++ app/models/ip_range.rb
@@ -5,4 +5,17 @@
 # ==============================================================================
 class IpRange < ApplicationRecord
   include Ip # Includes custom class methods
+
+  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+  @network_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$/
+  validates_presence_of :service_id
+  validates :network,
+            presence: true,
+            uniqueness: true,
+            format: { multiline: true, with: @network_regex }
+
+  validates :gateway,
+            presence: true,
+            uniqueness: true,
+            format: { multiline: true, with: @ip_regex }
 end
