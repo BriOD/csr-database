5bb30c47853c2abc5d14336f28fb76bb87482f46
arturusfury <bri.reynolds@gmail.com>
2016-08-21 23:47:40 -0500

    added specs for the ip address model

--- spec/factories/ip_addresses.rb
+++ spec/factories/ip_addresses.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
-  factory :ip_address do
-    
+  factory :ip_address do |f|
+    f.ip_range_id 1
+    f.customer_id nil
+    f.ip '192.168.0.2'
+    f.reserved false
   end
 end
--- spec/models/ip_address_spec.rb
+++ spec/models/ip_address_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe IpAddress, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validation' do
+    let(:service) { FactoryGirl.create(:service) }
+    let(:ip_range) { FactoryGirl.create(:ip_range, service_id: service.id) }
+
+    it 'has a valid factory' do
+      expect(FactoryGirl.create(:ip_address, ip_range_id: ip_range.id)).to be_valid
+    end
+
+    it 'has a unique ip' do
+      FactoryGirl.create(:ip_address, ip_range_id: ip_range.id)
+      expect(FactoryGirl.build(:ip_address, ip_range_id: ip_range.id)).to_not be_valid
+    end
+
+    it 'must contain a ip_range_id' do
+      expect(FactoryGirl.build(:ip_address, ip_range_id: nil)).to_not be_valid
+    end
+
+    it 'can contain a customer_id'
+    it 'can be reserved'
+  end
 end
