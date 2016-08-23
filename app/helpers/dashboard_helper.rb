0c3e24dee317bd032a2c8ed78c3cbda8c2b77883
arturusfury <bri.reynolds@gmail.com>
2016-08-23 09:29:54 -0500

    updated controller name to match my ip_range model

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,21 +4,21 @@ module DashboardHelper
     ranges = IpRange.where(service_id: service.id)
 
     if ranges.size > 1
-      '<li class="submenu">'
-      '<a href=""><i class="fa fa-desktop"></i> <span>DSL</span> <i class="arrow fa fa-chevron-right"></i></a>'
-      '<ul>'
+      # '<li class="submenu">'.html_safe
+      # '<a href=""><i class="fa fa-desktop"></i> <span>DSL</span> <i class="arrow fa fa-chevron-right"></i></a>'.html_safe
+      # '<ul>'.html_safe
 
       ranges.each do |range|
-        content_for(':li', link_to(range.network, iprange_path(range)))
+        content_for(':li', link_to(range.network, ip_range_path(range)))
       end
 
-      '</ul>'.html_safe
-      '</li>'.html_safe
+      # '</ul>'.html_safe
+      # '</li>'.html_safe
     else
       range = ranges.first
       link_to range do
-        fa_icon range.service.icon
-        content_tag(:span, range.network)
+        fa_icon 'ban'
+        content_tag(:span, range.network_name)
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :iprange, only: [:show] do
+  resources :ip_range, only: [:show] do
     resources :ipaddress, only: [:show]
   end
 
