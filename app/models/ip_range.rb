f8137d7c2b72f50b5a0b95aea09bcfa071d474a6
Brian Reynolds <bri.reynolds@gmail.com>
2016-08-23 21:54:41 +0000

    removed validations that were not working correctly, will add back in later

--- app/models/ip_range.rb
+++ app/models/ip_range.rb
@@ -8,18 +8,13 @@ class IpRange < ApplicationRecord
   belongs_to :service
   has_many :ip_addresses
 
-  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
-  @network_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$/
-
   validates :network,
             presence: true,
-            uniqueness: true,
-            format: { multiline: true, with: @network_regex }
+            uniqueness: true
 
   validates :gateway,
             presence: true,
-            uniqueness: true,
-            format: { multiline: true, with: @ip_regex }
+            uniqueness: true
 
   validates_presence_of :service_id
 
