734d8905846c81651028a75d057391d0b44bc53f
arturusfury <bri.reynolds@gmail.com>
2016-08-23 19:22:37 -0500

    included the new sidebar helper so it can be used other places

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   layout :layout_by_resource
 
+  include SidebarHelper
+
   protected
 
   def layout_by_resource
