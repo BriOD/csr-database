6bf7b0ae70a2db9bd166061f07f9d792046136e4
arturusfury <bri.reynolds@gmail.com>
2016-08-23 21:00:39 -0500

    generated more dynamic content for the dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,2 +1,68 @@
 module DashboardHelper
+  def make_progress_bar(range)
+    # <div class="progress progresscustom progress-striped">
+    #   <div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="<%= range.percentage_used %>" aria-valuemin="0" aria-valuemax="100" style="width:<%= range.percentage_used %>%"></div>
+    # </div><!-- end progress bar -->
+    content_tag(:div, class: 'progress progresscustom progress-striped') do
+      percentage = range.percentage_used
+
+      if percentage > 90
+        content_tag(
+          :div,
+          nil,
+          class: 'progress-bar progress-bar-danger',
+          role: 'progressbar',
+          aria:
+          {
+            valuenow: range.percentage_used,
+            valuemin: '0',
+            valuemax: '100'
+          },
+          style: "width:#{range.percentage_used}%"
+        )
+      elsif percentage > 65
+        content_tag(
+          :div,
+          nil,
+          class: 'progress-bar progress-bar-warning',
+          role: 'progressbar',
+          aria:
+          {
+            valuenow: range.percentage_used,
+            valuemin: '0',
+            valuemax: '100'
+          },
+          style: "width:#{range.percentage_used}%"
+        )
+      elsif percentage > 45
+        content_tag(
+          :div,
+          nil,
+          class: 'progress-bar progress-bar-info',
+          role: 'progressbar',
+          aria:
+          {
+            valuenow: range.percentage_used,
+            valuemin: '0',
+            valuemax: '100'
+          },
+          style: "width:#{range.percentage_used}%"
+        )
+      else
+        content_tag(
+          :div,
+          nil,
+          class: 'progress-bar progress-bar-success',
+          role: 'progressbar',
+          aria:
+          {
+            valuenow: range.percentage_used,
+            valuemin: '0',
+            valuemax: '100'
+          },
+          style: "width:#{range.percentage_used}%"
+        )
+      end
+    end
+  end
 end
