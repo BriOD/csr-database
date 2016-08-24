9be4d4e9d830fbaaa862610612c23a028c50c51b
arturusfury <bri.reynolds@gmail.com>
2016-08-24 14:49:32 -0500

    updated customer model with missing account number

--- db/migrate/20160822045955_create_customers.rb
+++ db/migrate/20160822045955_create_customers.rb
@@ -3,6 +3,7 @@ class CreateCustomers < ActiveRecord::Migration[5.0]
     create_table :customers do |t|
       t.integer :ip_address_id
       t.integer :address_book_id
+      t.integer :account_number
       t.string :first_name
       t.string :last_name
       t.string :home_phone
