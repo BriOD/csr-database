d3cffd03d62eb75d4c09f6542645b3815bb587b7
arturusfury <bri.reynolds@gmail.com>
2016-08-24 15:12:02 -0500

    attempted to use more helper methods with no success

--- app/helpers/iprange_helper.rb
+++ app/helpers/iprange_helper.rb
@@ -9,24 +9,28 @@ module IprangeHelper
     end
   end
 
-  def make_range_table_entry(ip_address)
-    customer = ip_address.customer
+  def make_table_link(ip_addr)
+    link_to(ip_addr.ip, iprange_ipaddress_path(ip_addr.ip_range, ip_addr))
+  end
+
+  def make_range_table_entry(ip_addr)
+    customer = ip_addr.customer
 
     if customer.nil?
       content_tag(:tr) do
-        content_tag :td, link_to(ip_address.ip, iprange_ipaddress_path(ip_address.ip_range, ip_address))
-        content_tag :td, '----', class: 'hidden-xs'
-        content_tag :td, 'Unassigned'
-        content_tag :td, '----'
-        content_tag :td, make_tdnp_button(customer)
+        content_tag(:td, link_to(ip_addr.ip, iprange_ipaddress_path(ip_addr.ip_range, ip_addr)))
+        content_tag(:td, '----', class: 'hidden-xs')
+        content_tag(:td, 'Unassigned')
+        content_tag(:td, '----')
+        content_tag(:td, make_tdnp_button(customer))
       end
     else
       content_tag(:tr, class: ('tdnp' unless customer.active)) do
-        content_tag :td, link_to(ip_address.ip, iprange_ipaddress_path(ip_address.ip_range, ip_address))
-        content_tag :td, customer.account_number, class: 'hidden-xs'
-        content_tag :td, customer.name
-        content_tag :td, number_to_phone(customer.home_phone, area_code: true)
-        content_tag :td, make_tdnp_button(customer)
+        content_tag(:td, link_to(ip_addr.ip, iprange_ipaddress_path(ip_addr.ip_range, ip_addr)))
+        content_tag(:td, customer.account_number, class: 'hidden-xs')
+        content_tag(:td, customer.name)
+        content_tag(:td, number_to_phone(customer.home_phone, area_code: true))
+        content_tag(:td, make_tdnp_button(customer))
       end
     end
   end
