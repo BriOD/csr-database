9c8fc568dabea756d055ea46a8d4c57e6fecfe69
arturusfury <bri.reynolds@gmail.com>
2016-08-24 13:29:29 -0500

    fixed hash syntax

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,7 +5,6 @@
 # work_phone, notes, active, email
 # ==============================================================================
 class Customer < ApplicationRecord
-  # include Phone
   belongs_to :ip_address
   belongs_to :address_book
 
@@ -13,25 +12,21 @@ class Customer < ApplicationRecord
   has_one :company
   has_one :lease
 
-  validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
+  validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates :home_phone, numericality: { only_integer: true }
   validates :cell_phone, allow_blank: true, numericality: { only_integer: true }
   validates :work_phone, allow_blank: true, numericality: { only_integer: true }
 
-  # validate :name?
+  validate :name?
 
-  # def name?
-  #   if first_name.nil? && last_name.nil? && company.nil?
-  #     errors.add(:base, 'You must have either a first and last name or company')
-  #   end
-  # end
+  def name?
+    if first_name.nil? && last_name.nil? && company.nil?
+      errors.add(:base, 'You must have either a first and last name or company')
+    end
+  end
 
   def name
     "#{first_name} #{last_name}"
-  end
-
-  def home_phone
-    # number_to_phone(home_phone, area_code: true)
   end
 
   def self.disconnected
