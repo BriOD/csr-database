0a84b74bf57a15e429c2797aea3fe772a11eb201
arturusfury <bri.reynolds@gmail.com>
2016-08-25 13:10:57 -0500

    added test for .name

--- spec/models/ip_range_spec.rb
+++ spec/models/ip_range_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe IpRange, type: :model do
+  let(:service) { FactoryGirl.create(:service) }
+  
   describe 'validation' do
-    let(:service) { FactoryGirl.create(:service) }
-
     it 'has a valid factory' do
       expect(FactoryGirl.create(:ip_range, service_id: service.id)).to be_valid
     end
@@ -42,13 +42,17 @@ RSpec.describe IpRange, type: :model do
         expect(FactoryGirl.build(:ip_range, network: '192.168.5.1/24', gateway: '192.168.5.1', service_id: service.id)).to be_valid
       end
     end
+  end
 
-    describe 'methods' do
-      it '.name shows the name of the ip range without the CIDR (/24, /28, etc)'
-      it '.subnet_mask shows an ip ranges subnet mask'
-      it '.unassigned returns a count of unassigned ip addresses in a range'
-      it '.assigned returns a count of assigned ip addresses in a range'
-      it '.percentage_used returns the percentage of a range that has been assigned'
+  describe 'methods' do
+    it '.name shows the name of the ip range without the CIDR (/24, /28, etc)' do
+      ip_range = FactoryGirl.build(:ip_range, network: '192.168.0.1/24', service_id: service.id)
+      expect(ip_range.name).to eq('192.168.0.0')
     end
+
+    it '.subnet_mask shows an ip ranges subnet mask'
+    it '.unassigned returns a count of unassigned ip addresses in a range'
+    it '.assigned returns a count of assigned ip addresses in a range'
+    it '.percentage_used returns the percentage of a range that has been assigned'
   end
 end
