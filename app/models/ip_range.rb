e6f92b150f57b4638f21c6c3bb22c9db540ac11e
arturusfury <bri.reynolds@gmail.com>
2016-08-25 15:19:27 -0500

    cleaned up a few methods and added the create_ip_addresses method

--- app/models/ip_range.rb
+++ app/models/ip_range.rb
@@ -13,13 +13,13 @@ class IpRange < ApplicationRecord
 
   validates :network,
             presence: true,
-            uniqueness: true,
             format: { multiline: true, with: @network_regex }
 
   validates :gateway,
             presence: true,
-            uniqueness: true,
             format: { multiline: true, with: @ip_regex }
+
+  validate :unique_ips?
 
   validates_presence_of :service_id
 
@@ -42,5 +42,28 @@ class IpRange < ApplicationRecord
   def percentage_used
     return 0 if assigned.zero?
     (assigned.to_f / ip_addresses.all.size.to_f * 100).to_i
+  end
+
+  def create_ip_addresses
+    ip_ary = NetAddr::CIDR.create(network).enumerate
+    ip_ary.shift(2) # Removes the first two addresses that cannot be assigned
+    ip_ary.pop(1) # Remove the last address that cannot be assigned
+    ip_ary.each do |ip_addr|
+      IpAddress.create(
+        ip_range_id: id,
+        customer_id: nil,
+        ip: ip_addr,
+        reserved: false
+      )
+    end
+  end
+
+  private
+
+  def unique_ips?
+    network_ranges = IpRange.all.map { |range| range.network }
+    gateway_ranges = IpRange.all.map { |range| range.gateway }
+    errors.add(:base, 'must have a unique ip range') if network_ranges.include?(network)
+    errors.add(:base, 'must have a unique ip gateway') if gateway_ranges.include?(gateway)
   end
 end
