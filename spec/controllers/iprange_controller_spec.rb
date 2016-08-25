eb3b04a96d5e5ad882d8e75f62962fbdd6561b7a
arturusfury <bri.reynolds@gmail.com>
2016-08-25 15:58:30 -0500

    updated test coverage

--- spec/controllers/iprange_controller_spec.rb
+++ spec/controllers/iprange_controller_spec.rb
@@ -6,45 +6,47 @@ RSpec.describe IprangeController, type: :controller do
       get :show
       expect(response).to have_http_status(:success)
     end
-
-    it 'displays the current ip range as a breadcrumb' do
-      #
-    end
-
-    it 'shows the subnet mask and gateway of the current range' do
-      #
-    end
-
-    it 'lists all of the ip addresses available in that range' do
-      #
-    end
-
-    context 'for a single customer' do
-      it 'shows their ip address' do
-        #
-      end
-
-      it 'shows their account number' do
-        #
-      end
-
-      it 'shows their full name' do
-        #
-      end
-
-      it 'shows their home phone number' do
-        #
-      end
-
-      context 'shows a button' do
-        it 'to disconnect if the customer is active' do
-          #
-        end
-
-        it 'to reconnect if the customer is inactive' do
-          #
-        end
-      end
-    end
   end
 end
+#
+#     it 'displays the current ip range as a breadcrumb' do
+#       #
+#     end
+#
+#     it 'shows the subnet mask and gateway of the current range' do
+#       #
+#     end
+#
+#     it 'lists all of the ip addresses available in that range' do
+#       #
+#     end
+#
+#     context 'for a single customer' do
+#       it 'shows their ip address' do
+#         #
+#       end
+#
+#       it 'shows their account number' do
+#         #
+#       end
+#
+#       it 'shows their full name' do
+#         #
+#       end
+#
+#       it 'shows their home phone number' do
+#         #
+#       end
+#
+#       context 'shows a button' do
+#         it 'to disconnect if the customer is active' do
+#           #
+#         end
+#
+#         it 'to reconnect if the customer is inactive' do
+#           #
+#         end
+#       end
+#     end
+#   end
+# end
