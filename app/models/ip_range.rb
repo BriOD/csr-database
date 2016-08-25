a0aac779b9113baf2619ae013adf280498a5b3e7
arturusfury <bri.reynolds@gmail.com>
2016-08-25 12:41:57 -0500

    added back in regex validations

--- app/models/ip_range.rb
+++ app/models/ip_range.rb
@@ -8,13 +8,19 @@ class IpRange < ApplicationRecord
   has_many :ip_addresses
   has_many :customers, through: :ip_addresses
 
+  @ip_regex = /^([1-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(\.([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])){3}$/
+  @network_regex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))$/
+
+
   validates :network,
             presence: true,
-            uniqueness: true
+            uniqueness: true,
+            format: { multiline: true, with: @network_regex }
 
   validates :gateway,
             presence: true,
-            uniqueness: true
+            uniqueness: true,
+            format: { multiline: true, with: @ip_regex }
 
   validates_presence_of :service_id
 
