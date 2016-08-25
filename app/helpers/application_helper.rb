984ab351e236bb478d8005db473e39988e3f2537
arturusfury <bri.reynolds@gmail.com>
2016-08-25 16:54:18 -0500

    added a breadcrumb link helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def make_breadcrumb_link(icon, path, link_text, css_class = nil, title = nil)
+    link_to(path, class: css_class, title: title) do
+      "#{fa_icon(icon)} #{link_text}".html_safe
+    end
+  end
 end
