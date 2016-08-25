8dd2c4ec11f38474253cc91c89b145313116cbc0
arturusfury <bri.reynolds@gmail.com>
2016-08-25 15:18:49 -0500

    added more test coverage

--- spec/factories/ip_ranges.rb
+++ spec/factories/ip_ranges.rb
@@ -2,8 +2,8 @@ require 'faker'
 
 FactoryGirl.define do
   factory :ip_range do |f|
-    f.network '192.168.0.2/24'
-    f.gateway '192.168.0.1'
+    f.network '192.168.254.2/24'
+    f.gateway '192.168.254.1'
     f.service_id 2
   end
 end
