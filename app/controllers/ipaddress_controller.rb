f74cbf980e53b0574f4c676f17c7104f088d7440
arturusfury <bri.reynolds@gmail.com>
2016-08-25 16:54:34 -0500

    added controller actions

--- app/controllers/ipaddress_controller.rb
+++ app/controllers/ipaddress_controller.rb
@@ -1,2 +1,27 @@
 class IpaddressController < ApplicationController
+  # Create
+  def new
+  end
+
+  def create
+  end
+
+  # Read
+  def show
+    @ip_address = IpAddress.find(params[:id])
+    @customer = @ip_address.customer
+
+    @customer.new if @customer.nil?
+  end
+
+  # Update
+  def edit
+  end
+
+  def update
+  end
+
+  # Destroy
+  def destroy
+  end
 end
