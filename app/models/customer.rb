a71bc3feb021f1d0353ed95350902127e9bb70b1
arturusfury <bri.reynolds@gmail.com>
2016-08-28 21:40:00 -0500

    added custom writers for phone numbers to strip characters and custom error message

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -20,14 +20,17 @@ class Customer < ApplicationRecord
   accepts_nested_attributes_for :webspace
 
   validates_format_of :email, with: /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
-  # validates :home_phone, numericality: { only_integer: true }
-  # validates :cell_phone, allow_blank: true, numericality: { only_integer: true }
-  # validates :work_phone, allow_blank: true, numericality: { only_integer: true }
+  validates :home_phone, numericality: { only_integer: true, message: 'must be filled in' }
+  validates :cell_phone, allow_blank: true, numericality: { only_integer: true }
+  validates :work_phone, allow_blank: true, numericality: { only_integer: true }
 
   validate :name?
 
+  NULL_ATTRS = %w(first_name last_name email home_phone cell_phone work_phone)
+  before_save :blank_if_nil
+
   def name?
-    if first_name.nil? && last_name.nil? && company.nil?
+    if first_name == '' && last_name == '' && company.id == ''
       errors.add(:base, 'You must have either a first and last name or company')
     end
   end
@@ -38,5 +41,25 @@ class Customer < ApplicationRecord
 
   def self.disconnected
     where(active: false)
+  end
+
+  def blank_if_nil
+    NULL_ATTRS.each { |attr| self[attr] = nil if self[attr].blank? }
+  end
+
+  def home_phone=(val)
+    self['home_phone'] = strip_phone(val)
+  end
+
+  def cell_phone=(val)
+    self['cell_phone'] = strip_phone(val) unless val.nil?
+  end
+
+  def work_phone=(val)
+    self['work_phone'] = strip_phone(val) unless val.nil?
+  end
+
+  def strip_phone(phone_number)
+    phone_number.gsub(/\D/, '')
   end
 end
