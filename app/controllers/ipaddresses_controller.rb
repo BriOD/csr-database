4ddaa3d62924830e4216749ea0e2235862775c23
arturusfury <bri.reynolds@gmail.com>
2016-08-29 15:43:39 -0500

    changed customer to either find a customer or build a new one from our ip address

--- app/controllers/ipaddresses_controller.rb
+++ app/controllers/ipaddresses_controller.rb
@@ -4,7 +4,7 @@ class IpaddressesController < ApplicationController
   def show
     @ip_address = IpAddress.find(params[:id])
     @ip_range = @ip_address.ip_range
-    @customer = @ip_address.customer || Customer.new
+    @customer = @ip_address.customer || @ip_address.build_customer
 
     @company = @customer.company || @customer.build_company
     @company_address = @company.company_address || @company.build_company_address
