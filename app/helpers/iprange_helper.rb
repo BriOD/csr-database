8ae5c3367dd7cd9b8923a32f016aa9a87984dae4
arturusfury <bri.reynolds@gmail.com>
2016-08-28 19:11:53 -0500

    moved encode address to ipaddress_helper instead of iprange_helper

--- app/helpers/iprange_helper.rb
+++ app/helpers/iprange_helper.rb
@@ -34,22 +34,4 @@ module IprangeHelper
       end
     end
   end
-
-  def encode_address(customer)
-    if customer.nil? || customer.id.nil?
-      '211 South Main Street, Auxvasse, MO 65231'
-    else
-      address = customer.address_book.address_1.to_s
-
-      unless customer.address_book.address_2.empty?
-        address << ", #{customer.address_book.address_2}"
-      end
-
-      address << ", #{customer.address_book.city}"
-      address << ", #{customer.address_book.state}"
-      address << ", #{customer.address_book.zipcode}"
-
-      address
-    end
-  end
 end
