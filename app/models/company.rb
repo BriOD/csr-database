af95fcc1c2c1ad892075ca5af911246585f9a448
arturusfury <bri.reynolds@gmail.com>
2016-08-28 19:58:15 -0500

    changed association to make it easier to use nested attributes

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,10 +5,11 @@
 # address_book_id, main_number, contact_number, fax
 # ==============================================================================
 class Company < ApplicationRecord
-  validates :main_number, allow_blank: true, numericality: { only_integer: true }
-  validates :contact_number, numericality: { only_integer: true }
-  validates :fax, allow_blank: true, numericality: { only_integer: true }
+  # validates :main_number, allow_blank: true, numericality: { only_integer: true }
+  # validates :contact_number, numericality: { only_integer: true }
+  # validates :fax, allow_blank: true, numericality: { only_integer: true }
 
   belongs_to :customer
-  belongs_to :address_book
+  belongs_to :company_address, class_name: 'AddressBook', foreign_key: 'address_book_id'
+  accepts_nested_attributes_for :company_address
 end
