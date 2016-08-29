1b7affeefb02ba93cce3b9d56c6ab8704182c061
arturusfury <bri.reynolds@gmail.com>
2016-08-29 10:44:30 -0500

    improved our validation messages and any other flash message styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,17 @@ module ApplicationHelper
       "#{fa_icon(icon)} #{link_text}".html_safe
     end
   end
+
+  def get_alert_type(type)
+    case type.to_sym
+    when :alert, :danger, :error, :validation_errors
+      alert_type = 'alert-danger'
+    when :warning, :todo
+      alert_type = 'alert-warning'
+    when :notice, :success
+      alert_type = 'alert-success'
+    else
+      alert_type = 'alert-info'
+    end
+  end
 end
