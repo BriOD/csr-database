2546af23d286a384510092693ed58d88ebe88bff
Brian Reynolds <bri.reynolds@gmail.com>
2016-10-14 20:57:12 +0000

    updated spacing

--- config/routes.rb
+++ config/routes.rb
@@ -5,29 +5,28 @@ Rails.application.routes.draw do
   
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      resources :companies, only: [:show, :index]
-      resources :customers, only: [:show, :index]
-      resources :ipaddresses, only: [:show, :index]
-      resources :ipranges, only: [:show, :index]
-      resources :leases, only: [:show, :index]
-      resources :webspaces, only: [:show, :index]
+      resources :companies,     only: [:show, :index]
+      resources :customers,     only: [:show, :index]
+      resources :ipaddresses,   only: [:show, :index]
+      resources :ipranges,      only: [:show, :index]
+      resources :leases,        only: [:show, :index]
+      resources :webspaces,     only: [:show, :index]
     end
   end
 
-  patch 'customers/:id/move' => 'customers#move', as: :customer_move
-  get 'customers/:id/tdnp' => 'customers#tdnp', as: :customer_tdnp
-  get 'customers/tdnp' => 'customers#show_tdnps', as: :customer_show_tdnps
+  patch  'customers/:id/move'     => 'customers#move',              as: :customer_move
+  get    'customers/:id/tdnp'     => 'customers#tdnp',              as: :customer_tdnp
+  get    'customers/tdnp'         => 'customers#show_tdnps',        as: :customer_show_tdnps
+  get    'ipaddresses/unassigned' => 'ipaddresses#show_unassigned', as: :ipaddresses_show_unassigned
+  
+  get    '/dashboard',            => 'dashboard#index'
+  get    '/settings',             => 'dashboard#settings'
+  root   'dashboard#index'
 
-  get 'ipaddresses/unassigned' => 'ipaddresses#show_unassigned', as: :ipaddresses_show_unassigned
-
-  resources :customers, only: [:create, :update, :destroy]
-  resources :companies, only: [:create, :update, :destroy]
-  resources :leases, only: [:create, :update, :destroy]
-  resources :webspaces, only: [:create, :update, :destroy]
-
-  root 'dashboard#index'
-  get '/dashboard', to: 'dashboard#index'
-  get '/settings', to: 'dashboard#settings'
+  resources :customers,   only: [:create, :update, :destroy]
+  resources :companies,   only: [:create, :update, :destroy]
+  resources :leases,      only: [:create, :update, :destroy]
+  resources :webspaces,   only: [:create, :update, :destroy]
 
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
