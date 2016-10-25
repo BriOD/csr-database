36e47e1cbcc1c92613a13ef491ffec1909b5d6a8
arturusfury <bri.reynolds@gmail.com>
2016-10-25 10:44:12 -0500

    removed spacing

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::CustomersController < ApplicationController
   before_action :set_api_v1_customer, only: [:show]
-  
+
   respond_to :json
-  
+
   def index
     @customers = Customer.order(:id)
     render json: @customers, include: ['']
   end
-  
+
   def show
     render json: @api_v1_customer, include: ['address_book','webspace','company','lease']
   end
-  
+
   def show_tdnps
     results = Customer.where(active: false).includes([:ip_address])
     render json: results, include: ['']
   end
-  
+
   private
-  
+
   def set_api_v1_customer
     @api_v1_customer = Customer.find(params[:id])
   end
