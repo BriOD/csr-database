a8cade5341bee8a78270d95b37f960f52b9a37d2
arturusfury <bri.reynolds@gmail.com>
2016-10-25 10:45:44 -0500

    added some nested information to our serializer

--- app/controllers/api/v1/ipranges_controller.rb
+++ app/controllers/api/v1/ipranges_controller.rb
@@ -1,19 +1,23 @@
 class Api::V1::IprangesController < ApplicationController
   before_action :set_api_v1_iprange, only: [:show]
-  
+
   respond_to :json
-  
+
   def index
     @ipranges = IpRange.order(:id)
     render json: @ipranges, include: ['']
   end
-  
+
   def show
-    render json: @api_v1_iprange, include: ['ip_addresses']
+    render json: @api_v1_iprange,
+           include: [
+             'ip_addresses',
+             'ip_addresses.customer'
+           ]
   end
-  
+
   private
-  
+
   def set_api_v1_iprange
     @api_v1_iprange = IpRange.find(params[:id])
   end
