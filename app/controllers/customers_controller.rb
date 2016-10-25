f00bc94bd7d70bf4a0f57b4e92bd1190f56b08ba
arturusfury <bri.reynolds@gmail.com>
2016-10-25 10:43:33 -0500

    removed some extra spacing

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -11,10 +11,10 @@ class CustomersController < ApplicationController
 
     if customer.errors.empty?
       customer.ip_address.update(reserved: true)
-      flash[:notice] = 'Customer information has been added successfully'
+      # flash[:notice] = 'Customer information has been added successfully'
       redirect_to iprange_ipaddress_path(customer.ip_range, customer.ip_address)
     else
-      flash[:error] = customer.errors.full_messages
+      # flash[:error] = customer.errors.full_messages
       redirect_back(fallback_location: root_path)
     end
   end
@@ -38,10 +38,10 @@ class CustomersController < ApplicationController
 
     if customer.errors.empty?
       customer.ip_address.update(reserved: true)
-      flash[:notice] = 'Customer information has been updated successfully'
+      # flash[:notice] = 'Customer information has been updated successfully'
       redirect_to iprange_ipaddress_path(customer.ip_range, customer.ip_address)
     else
-      flash[:error] = customer.errors.full_messages
+      # flash[:error] = customer.errors.full_messages
       redirect_back(fallback_location: root_path)
     end
   end
@@ -50,7 +50,7 @@ class CustomersController < ApplicationController
     @customer = Customer.find(params[:id])
     ip_range = @customer.ip_range
     ip_address = @customer.ip_address
-    
+
     ip_address.reserved = false
 
     @customer.company.company_address.destroy unless @customer.company.nil?
@@ -98,7 +98,7 @@ class CustomersController < ApplicationController
 
   def customer_params
     params.require(:customer).permit(
-      :first_name, :last_name, :email, :home_phone, :cell_phone, :work_phone,
+      :id, :first_name, :last_name, :email, :home_phone, :cell_phone, :work_phone,
       :notes, :lease_checkbox, :webspace_checkbox, :id, :ip_address_id, :account_number,
       address_book_attributes: [:id, :address_1, :address_2, :city, :state, :zipcode]
     )
