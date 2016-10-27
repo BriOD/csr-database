ee8248914bd56287171516bcb4a62e86924817fb
arturusfury <bri.reynolds@gmail.com>
2016-10-26 20:51:41 -0500

    added nested items to our api call for customer info

--- app/controllers/api/v1/ipaddresses_controller.rb
+++ app/controllers/api/v1/ipaddresses_controller.rb
@@ -1,24 +1,33 @@
 class Api::V1::IpaddressesController < ApplicationController
   before_action :set_api_v1_ipaddresses, only: [:show]
-  
+
   respond_to :json
-  
+
   def index
     @ipaddresses = IpAddress.order(:id)
     render json: @ipaddresses, include: ['']
   end
-  
+
   def show
-    render json: @api_v1_ipaddress, include: ['customer']
+    render json: @api_v1_ipaddress,
+           include:
+             [
+               'customer',
+               'customer.address_book',
+               'customer.company',
+               'customer.comapny.company_address',
+               'customer.webspace',
+               'customer.lease'
+             ]
   end
-  
+
   def show_unassigned
     results = IpAddress.order(:id).includes(:ip_range).includes(:customer).where(reserved: false)
     render json: results
   end
-  
+
   private
-  
+
   def set_api_v1_ipaddresses
     @api_v1_ipaddress = IpAddress.find(params[:id])
   end
